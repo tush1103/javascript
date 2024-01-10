dd44ed7466ac7c9640c919cd2b31d7eeaa37a556
Tushika <tushikagarg11@gmail.com>
2024-01-10 19:35:56 +0530

    math and Number

---
+++ numbers.js
@@ -0,0 +1,16 @@
+const score=400
+console.log(score)
+
+const balance=new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const othernumber=123.8966
+console.log(othernumber.toPrecision(3))
+console.log(othernumber.toPrecision(4))
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)

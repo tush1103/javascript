d0c31c15730161fb8dda645d64d170df601cee1d
Tushika <tushikagarg11@gmail.com>
2024-01-10 16:09:34 +0530

    conversions

---
+++ conversionOperations/conversion.js
@@ -0,0 +1,53 @@
+let score1="33abc";
+
+console.log(typeof(score1));
+
+let valueInNumber=Number(score1);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+let score2=null;
+
+console.log(typeof(score2));
+
+let valueInNumber2=Number(score2);
+console.log(typeof(valueInNumber2));
+
+console.log(valueInNumber2);
+
+let score3=undefined;
+
+console.log(typeof(score3));
+
+let valueInNumber3=Number(score3);
+console.log(typeof(valueInNumber3));
+
+console.log(valueInNumber3);
+
+
+let score4=true;
+
+console.log(typeof(score4));
+
+let valueInNumber4=Number(score4);
+console.log(typeof(valueInNumber4));
+
+console.log(valueInNumber4);
+
+//conversion to number
+// "33" => 33
+// null => 0
+// "33abc" => NaN
+// undefined => NaN
+// true => 1 ; false => 0
+
+
+//conversion to boolean
+/*
+
+1 => true; 0 => false
+"" => false
+"hitesh" => true
+
+*/

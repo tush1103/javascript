8289f10d779dd7153063cdd54f9117cdada971b2
Tushika <tushikagarg11@gmail.com>
2024-01-13 00:35:51 +0530

    arrow function

---
+++ arrowFunc.js
@@ -0,0 +1,20 @@
+// console.log(this); //{}
+// function chai(){
+//     console.log(this);//smething large output
+//      let username="tush"
+//      console.log(this.username);//undefined
+//      console.log(username);//tush
+//  }
+
+// chai()
+
+// const chai2=()=>{
+//     let username="tushi"
+//     console.log(this.username);//undefined
+//     console.log(this);//{}
+// }
+// chai2()
+
+
+const add=(n1,n2)=>(n1+n2)//()->no return statemt,{}->return statement should be used
+console.log(add(3,4));

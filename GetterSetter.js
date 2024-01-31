9ce0a2279fe637f246bc9256fc82ee9d88ee814c
Tushika <tushikagarg11@gmail.com>
2024-01-31 19:31:02 +0530

    getter setter

--- GetterSetter.js
+++ GetterSetter.js
@@ -1,1 +1,15 @@
- 
+ class User{
+    constructor(email,password){
+        this.email=email
+        this.password=password
+    }
+    get password(){
+        return `${this._password}hitesh`
+    }
+    //getter define kiya hai toh setter bhi define krna hi pdega
+    set password(value){
+        this._password=value
+    }
+ }
+ const tush=new User('tush@gmail.com','abc')
+ console.log(tush.password);
